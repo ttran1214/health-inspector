1b5acea35cffacde39aa5e023274838ea39ba273
Truc Tran <tructran@dhcp-v109-165.mobile.uci.edu>
2017-10-02 20:58:59 -0700

    made a hello world component

--- src/index.js
+++ src/index.js
@@ -3,4 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const App = () => {
+
+    //this is important this is not javascript, this is JSX
+    return (
+        <div>
+            <h1>Hello there World!</h1>
+            <p> I love saying hello world </p>
+        </div>
+    )
+}
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
